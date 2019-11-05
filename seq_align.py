0cd41db5e095f3a7f7ed7ad9b32bb3a1d4b7d664
Matanhalfon <39224350+Matanhalfon@users.noreply.github.com>
2019-11-05 21:19:07 +0200

    global finished

--- seq_align.py
+++ seq_align.py
@@ -14,6 +14,8 @@ GAP_IN_B = 2
 DIAG = 3
 GAP = "-"
 INITIAL_STR = ""
+GLOBAL = "global"
+LOCAL = "local"
 
 
 def fastaread(fasta_name):
@@ -42,16 +44,15 @@ def parseScoreMat(path):
 
 
 def getDiagScore(scoreTable, i, j, scoreDict, seqA, seqB):
-    return scoreDict[seqA[i - 1] + seqB[j - 1]]+scoreTable[i - 1][j - 1]
-
+    return scoreDict[seqA[i - 1] + seqB[j - 1]] + scoreTable[i - 1][j - 1]
 
 
 def getGapInAScore(scoreTable, i, j, scoreDict, seqB):
-    return scoreTable[i][j - 1] + scoreDict["-" + seqB[j-1]]
+    return scoreTable[i][j - 1] + scoreDict["-" + seqB[j - 1]]
 
 
 def getGapInBScore(scoreTable, i, j, scoreDict, seqA):
-    return scoreTable[i - 1][j] + scoreDict["-" + seqA[i-1]]
+    return scoreTable[i - 1][j] + scoreDict["-" + seqA[i - 1]]
 
 
 def getFlag(val, diagScore, gapInAScore, gapInBScore):
@@ -62,36 +63,34 @@ def getFlag(val, diagScore, gapInAScore, gapInBScore):
     elif (val == gapInBScore):
         return GAP_IN_B
 
+
 def parseSeg(path):
-    seq_record=list(SeqIO.parse(path,"fasta"))[0]
+    seq_record = list(SeqIO.parse(path, "fasta"))[0]
     return seq_record.seq
 
 
-def traceBackTable(flagTable,seqA, seqB):
+def traceBackTable(flagTable, seqA, seqB,row,col):
     alingedSeqA = INITIAL_STR
     alingedSeqB = INITIAL_STR
-    current = flagTable[-1][-1]
-    row ,col= len(seqA),len(seqB)
-    while current!=0:
-        if(current==DIAG):
-            alingedSeqA+=seqA[-1]
-            seqA = seqA[:-1]
-            alingedSeqB += seqB[-1]
-            seqB = seqB[:-1]
-            row-=1
-            col-=1
-        elif(current == GAP_IN_B):
-            alingedSeqA+=seqA[-1]
-            seqA = seqA[:-1]
+    current = flagTable[row][col]
+    while current != 0:
+        if (current == DIAG):
+            alingedSeqA += seqA[row-1]
+            alingedSeqB += seqB[col-1]
+            row -= 1
+            col -= 1
+        elif (current == GAP_IN_B):
+            alingedSeqA += seqA[row-1]
             alingedSeqB += GAP
-            row-=1
-        elif(current== GAP_IN_A):
-            alingedSeqA+=GAP
-            alingedSeqB += seqB[-1]
-            seqB = seqB[:-1]
-            col-=1
+            row -= 1
+        elif (current == GAP_IN_A):
+            alingedSeqA += GAP
+            alingedSeqB += seqB[col-1]
+            col -= 1
         current = flagTable[row][col]
     return alingedSeqA[::-1], alingedSeqB[::-1]
+
+
 
 def printResult(seqA, seqB, score, alignmentType):
     print(seqA)
@@ -99,7 +98,18 @@ def printResult(seqA, seqB, score, alignmentType):
     print(alignmentType + ":" + str(score))
 
 
-def getGlobalScore(seqA, seqB, scoreDict):
+def globalMAT(scoreTable,flagsTable, seqA, seqB):
+    for i in range(1, len(seqA) + INITIATION_LINE):
+        scoreTable[i][0] = scoreTable[i - 1][0] + scoreDict["-" + seqA[i - 1]]
+    for i in range(1, len(seqB) + INITIATION_LINE):
+        scoreTable[0][i] = scoreTable[0][i - 1] + scoreDict["-" + seqB[i - 1]]
+    flagsTable[0] = GAP_IN_A
+    for i in range(1, len(seqA) + INITIATION_LINE):
+        flagsTable[i][0] = GAP_IN_B
+    flagsTable[0][0] = 0
+
+
+def getGlobalScore(seqA, seqB, scoreDict, AlinmentType):
     '''
     :param seqA: first DNA sequence
     :param seqB: second DNA sequence
@@ -107,26 +117,25 @@ def getGlobalScore(seqA, seqB, scoreDict):
     :return: ??
     '''
     scoreTable = np.zeros((len(seqA) + INITIATION_LINE, len(seqB) + INITIATION_LINE))
-    for i in range(1,len(seqA)+INITIATION_LINE):
-        scoreTable[i][0]=scoreTable[i-1][0]+scoreDict["-"+seqA[i-1]]
-    for i in range(1,len(seqB)+INITIATION_LINE):
-        scoreTable[0][i]=scoreTable[0][i-1]+scoreDict["-"+seqB[i-1]]
-    flagsTable = np.zeros((len(seqA)+INITIATION_LINE, len(seqB)+INITIATION_LINE))
-    flagsTable[0]=GAP_IN_A
-    for i in range(1,len(seqA)+INITIATION_LINE):
-        flagsTable[i][0]=GAP_IN_B
-    flagsTable[0][0]=0
+    flagsTable = np.zeros((len(seqA) + INITIATION_LINE, len(seqB) + INITIATION_LINE))
+    if (AlinmentType == GLOBAL):
+        globalMAT(scoreTable,flagsTable, seqA, seqB)
     for j in range(1, len(seqB) + INITIATION_LINE):
         for i in range(1, len(seqA) + INITIATION_LINE):
             diagScore = getDiagScore(scoreTable, i, j, scoreDict, seqA, seqB)
             gapInAScore = getGapInAScore(scoreTable, i, j, scoreDict, seqB)
             gapInBScore = getGapInBScore(scoreTable, i, j, scoreDict, seqA)
             scoreTable[i][j] = max(diagScore, gapInAScore, gapInBScore)
-            flagsTable[i ][j] = getFlag(scoreTable[i][j],diagScore,gapInAScore,gapInBScore )
-    print(scoreTable)
-    print()
-    print(flagsTable)
-    return scoreTable[-1][-1], flagsTable
+            flagsTable[i][j] = getFlag(scoreTable[i][j], diagScore, gapInAScore, gapInBScore)
+    return scoreTable, flagsTable
+
+
+def findLocalScore(scoreTable):
+    scoreTable = np.delete(scoreTable, 0, axis=0)
+    scoreTable = np.delete(scoreTable, 0, axis=1)
+    max = np.max(scoreTable)
+    index = np.where(scoreTable == max)
+    return max, index[0][0]+INITIATION_LINE, index[1][0]+INITIATION_LINE
 
 
 def main():
@@ -137,8 +146,16 @@ def main():
     parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ',
                         default='score_matrix.tsv')
     command_args = parser.parse_args()
+    scoreDict = parseScoreMat("C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\score_matrix.tsv")
+    seqA = parseSeg(
+        "C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\fastas\\HelicoverpaArmigera-cMyc -A.fasta")
+    seqB = parseSeg(
+        "C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\fastas\\HelicoverpaArmigera-cMyc -B.fasta")
     if command_args.align_type == 'global':
-        raise NotImplementedError
+        scoreTable, flagTable = getGlobalScore(seqA, seqB, scoreDict, GLOBAL)
+        score = scoreTable[-1][-1]  # todo switch with alienment
+        seqA, seqB = traceBackTable(flagTable, seqA, seqB,len(seqA),len(seqB))
+        printResult(seqA, seqB, score, GLOBAL)
     elif command_args.align_type == 'local':
         raise NotImplementedError
     elif command_args.align_type == 'overlap':
@@ -147,8 +164,14 @@ def main():
 
 if __name__ == '__main__':
     scoreDict = parseScoreMat("C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\score_matrix.tsv")
-    seqA = parseSeg("C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\fastas\\HelicoverpaArmigera-cMyc -A.fasta")
-    seqB = parseSeg("C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\fastas\\HelicoverpaArmigera-cMyc -B.fasta")
-    score, flagTable=getGlobalScore(seqA,seqB,scoreDict)
-    seqA, seqB = traceBackTable(flagTable, seqA, seqB)
-    printResult(seqA, seqB, score, "global")
+    seqA = parseSeg(
+        "C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\fastas\\HelicoverpaArmigera-cMyc -A.fasta")
+    seqB = parseSeg(
+        "C:\\Users\\matan\\Desktop\\School\\bioAlgo\\bioAlgo-ex1\\fastas\\HelicoverpaArmigera-cMyc -B.fasta")
+    scoreTable, flagTable = getGlobalScore(seqA, seqB, scoreDict, LOCAL)
+    score, row, col = findLocalScore(scoreTable)
+    print(scoreTable)
+    print()
+    print(flagTable)
+    seqA, seqB = traceBackTable(flagTable, seqA, seqB,row,col)
+    printResult(seqA, seqB, score, LOCAL)
